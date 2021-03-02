5fcbc488e7cee6786c9c8ebde827ea4b583298e0
BibekMSDangol <78782357+BibekMSDangol@users.noreply.github.com>
2021-03-02 12:01:22 +0545

    initialcommit

---
+++ venv/Lab1/pythonfive.py
@@ -0,0 +1,17 @@
+''' A school decided to replace the desks in three classrooms. Each desks sits two students. Given the number of
+# students in each class, print the smallest possible number of desks that can be purchased.'''
+
+no_student_class1 = int(input("Enter the number of student in first class : "))
+no_student_class2 = int(input("Enter the number of students in second class : "))
+no_student_class3 = int(input("Enter the number of students in third class : "))
+
+desk_class1 = (no_student_class1 // 2)
+print(f"The required number of desk for first class is {desk_class1}")
+desk_class2 = (no_student_class2 // 2)
+print(f"The required number of desk for second class is {desk_class2}")
+desk_class3 = (no_student_class3 // 2)
+print(f"The required number of desk for third class is {desk_class3}")
+
+
+
+
