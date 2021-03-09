15701e0db531a689103ddf486d8b06fb7e3dfc47
BibekMSDangol <78782357+BibekMSDangol@users.noreply.github.com>
2021-03-09 12:11:48 +0545

    first commit

---
+++ venv/Lab1/pythoneight.py
@@ -0,0 +1,7 @@
+'''#Write a python program which accepts the radius of a circle from the user and compute the area.
+(area of  circle = Pi * r^2)'''
+
+radius = int(input("Enter the radius of a circle: "))
+area_of_circle = 3.14 * (radius**2)
+print(f"The area of a circle is {area_of_circle} ")
+
